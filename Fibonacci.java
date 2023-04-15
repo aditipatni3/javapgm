214bc6787947502b57c0457f4940c49d4cc6f461
aditipatni <aditipatni311@gmail.com>
2023-04-15 09:59:51 +0530

    demo

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n=in.nextInt();
+        int count=2;
+        int a=0;
+        int b=1;
+        if(n==1){
+            System.out.println(a);
+        }
+        else if(n==2){
+            System.out.println(b);
+        }
+
         
+        else{
+            
+        while(count<n){
+            int temp;
+            temp=b;
+            b=b+a;
+            a=temp;
+            count++;
+
+        }
+        System.out.println(b);
+        }
     }
 }
